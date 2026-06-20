b1462563643b87001f3e3feeee6f91615a08b3d1
Cleber <cleber@enviosegurobr.com>
2026-06-20 17:38:04 -0300

    fix: preview URL mostra verificadametta direto

--- frontend/src/components/blocks/InfraBlock.tsx
+++ frontend/src/components/blocks/InfraBlock.tsx
@@ -85,7 +85,7 @@ export default function InfraBlock({ clientId, razaoSocial, smsPhone, onDomainRe
   };
 
   const workerPreviewUrl = subdomain
-    ? `${subdomain}-(verificadametta|zaplifydisparo).workers.dev`
+    ? `${subdomain}-verificadametta.verificadametta.workers.dev`
     : '';
 
   return (
