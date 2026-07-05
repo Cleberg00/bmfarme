e2030a3cf0fc3fd7f7516fb508a4c1e1941b7869
Cleber <cleber@enviosegurobr.com>
2026-07-05 00:10:49 -0300

    feat: verificaperfil.com.br + verificabm.com.br wildcard DNS created, fix zone ID typo

--- frontend/src/components/blocks/InfraBlock.tsx
+++ frontend/src/components/blocks/InfraBlock.tsx
@@ -42,7 +42,7 @@ export default function InfraBlock({ clientId, razaoSocial, nomeFantasia, smsPho
   const netlifyDomains = isRonaldo
     ? ['verificametaativos.com', 'centralativoss.com', 'verificadapro1.com', 'masterverificada.com', 'verificaperfilbm.com', 'checkverifica.com.br']
     : isAdmin
-    ? ['verificativos.com', 'ativoscontas.com', 'verificacontas.com', 'zaplifyativos.com', 'zaplifynegocios.com', 'zaplifytrabalho.com', 'zaplifycontas.com', 'contaszaplify.com', 'farmezaplify.com', 'contasativas.com', 'zaplifybm.com.br', 'verificaativos.com', 'contasativasbr.com', 'verificazapli.com']
+    ? ['verificativos.com', 'ativoscontas.com', 'verificacontas.com', 'zaplifyativos.com', 'zaplifynegocios.com', 'zaplifytrabalho.com', 'zaplifycontas.com', 'contaszaplify.com', 'farmezaplify.com', 'contasativas.com', 'zaplifybm.com.br', 'verificaativos.com', 'contasativasbr.com', 'verificazapli.com', 'verificabm.com.br']
     : isMacumbinha
     ? ['contaszaplify.com', 'contasativasfb.com', 'verificaperfil01.com', 'verificacontas.com.br', 'verificativos.com', 'ativoscontas.com', 'verificacontas.com']
     : isZaplify
