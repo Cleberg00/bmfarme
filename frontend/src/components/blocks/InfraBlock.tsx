3c8808dde28a59eed4ec557c4c708d44afffac26
Cleber <cleber@enviosegurobr.com>
2026-07-18 16:47:50 -0300

    feat: adiciona veridesk1.com pro ronaldo

--- frontend/src/components/blocks/InfraBlock.tsx
+++ frontend/src/components/blocks/InfraBlock.tsx
@@ -40,7 +40,7 @@ export default function InfraBlock({ clientId, razaoSocial, nomeFantasia, smsPho
   const isZaplify = user?.email === 'julia@gmail.com' || user?.email === 'maria@gmail.com';
   const isMacumbinha = user?.email === 'miguelmacumbinha@gmail.com' || user?.email === 'macumbinha@gmail.com';
   const netlifyDomains = isRonaldo
-    ? ['mettaativos.com', 'verifcationbm.com.br', 'bmseven.com']
+    ? ['mettaativos.com', 'verifcationbm.com.br', 'bmseven.com', 'veridesk1.com']
     : isAdmin
     ? ['perfilbr.com', 'validarfm.com', 'verificabussines.com', 'verificadorbm.com', 'validacaopf.com', 'ativoson.com', 'verifcationbm.com', 'perfilbr01.com', 'vericationbm.com', 'zaplifyativos01.com', 'zaplifyvalidation.com', 'zaplify01.com', 'zaplifydigital.com']
     : isMacumbinha
