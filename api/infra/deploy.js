5925b33631f73a5e8c51707a564e3d709bbb3076
Cleber <cleber@enviosegurobr.com>
2026-07-25 00:34:06 -0300

    feat: adiciona zaplifymanager.com

--- api/infra/deploy.js
+++ api/infra/deploy.js
@@ -91,6 +91,7 @@ module.exports = async function handler(req, res) {
         'zaplifyfm.com': process.env.CLOUDFLARE_ZONE_ZAPLIFYFM,
         'bmzaplify10.com': process.env.CLOUDFLARE_ZONE_BMZAPLIFY10,
         'zaplifyflow.com': process.env.CLOUDFLARE_ZONE_ZAPLIFYFLOW,
+        'zaplifymanager.com': process.env.CLOUDFLARE_ZONE_ZAPLIFYMANAGER,
       };
 
       // Busca todos os domínios wildcard do usuário
