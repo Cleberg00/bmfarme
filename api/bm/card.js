aeb8c249e958e573df9ad0cb549365819a957f09
Cleber <cleber@enviosegurobr.com>
2026-08-17 12:54:20 -0300

    fix: email no comprovante em letra minuscula

--- api/bm/card.js
+++ api/bm/card.js
@@ -226,7 +226,7 @@ body{font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#000;background
 <tr class="info-row">
   <td width="50%" class="section">
     <span class="section-title">ENDEREÇO ELETRÔNICO</span>
-    <div class="section-data">${esc((d.email||'').toUpperCase())}</div>
+    <div class="section-data">${esc(d.email||'')}</div>
   </td>
   <td width="2%" class="section-no-border-vertical"></td>
   <td width="48%" class="section">
